2baa4f8daaed8b946780b93887e1a160925d31ba
Piovani <caiomelopiovani@gmail.com>
2025-02-05 19:14:10 -0300

    Exercicio 01

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+        System.out.println("Caio de Melo Piovani");
+        System.out.println("12411BCC085");
+    }
+}

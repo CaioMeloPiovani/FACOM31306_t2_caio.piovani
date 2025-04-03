998186be7eb0a31141aae73a1523f37de3128679
CaioMeloPiovani <135037766+CaioMeloPiovani@users.noreply.github.com>
2025-04-03 11:22:50 -0300

    Update Main.java

--- LAB07 - Exceptions/ex2/Main.java
+++ LAB07 - Exceptions/ex2/Main.java
@@ -2,12 +2,13 @@ package ex2;
 
 public class Main {
     public static void main(String[] args) {
-        Operacao[] operacaos = new Operacao[1];
+        Operacao[] operacaos = new Operacao[2];
 
 
         operacaos[0] = new Operacao(10, 2);
-
+        operacaos[1] = new Operacao(5, 0);
 
         operacaos[0].iniciarOperacao();
+        operacaos[1].iniciarOperacao();
     }
 }
